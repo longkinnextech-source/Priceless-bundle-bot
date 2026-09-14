424b8cb92ce821872af606c1dab3653bdfde3423
longkinnextech-source <longkinnex.tech@gmail.com>
2026-09-14 07:03:19 +0000

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -6,7 +6,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const DATA_DIR = path.join(__dirname, "..", "data");
+const DATA_DIR = path.join(__dirname, "data");
 const DATA_FILE = path.join(DATA_DIR, "wallets.json");
 
 function ensureStore() {
